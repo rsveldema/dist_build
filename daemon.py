06260e207127a8e8da86e951fe54cbee54781db0
rsveldema <rsveldema@gmail.com>
2021-07-04 18:03:47 +0200

    it now populates ~/dist_build via the daemon.

--- daemon.py
+++ daemon.py
@@ -1,9 +1,43 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from os import mkdir, getenv, path, makedirs
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello World!"
 
+def storage_dir():
+    home = getenv("HOME")
+    if home == None:
+        home = "c:/"
+    storage = home + '/dist_build'
+    return storage
+
+@app.route("/install_file", methods=['POST'])
+def install_file():
+    if request.method == 'POST':
+        pathprop = request.form.get('path')
+        content = request.form.get('content')
+
+        install_path = storage_dir() + pathprop
+        filename = path.basename(install_path)
+        install_dir = path.dirname(install_path).replace('/', '\\')
+
+        print('going to install ' + filename)
+        print(" AT  " + install_dir)
+
+        if not path.isdir(install_dir):
+            makedirs(install_dir)
+
+        fp=open(install_path, 'w')
+        fp.write(content)
+        fp.close()
+    else:
+        return "unhandled"
+    return "ok"
+
+
 if __name__ == "__main__":
     app.run(ssl_context=('cert.pem', 'key.pem'))
+
--- syncer.py
+++ syncer.py
@@ -1,13 +1,45 @@
 import json
+import requests
+import os
 
-def copy_over(dir: str):
-    pass
+header_suffix_list = ['.h', '.hh', '.hxx']
+
+def is_header_file(filename):
+    for h in header_suffix_list:
+        if filename.endswith(h):
+            return True
+    return False
+
+
+def install_directory(hosts: 'list[str]', dir: str):
+    content = os.listdir(dir)
+    print("content = " + str(content))
+    files = []
+    for item in content:
+        if is_header_file(item):
+            print('its a header: ' + item)
+            if os.path.isfile(dir + '/' + item):
+                files.append(item)
+
+    print("all files = " + str(files))
+    for filename in files:
+        for host in hosts:
+            uri = 'https://' + host + "/install_file"
+            path = dir + '/' + filename
+            fp = open(path)
+            content = fp.read()
+            fp.close()
+            r = requests.post(uri, data = {'path': path, 'content': content}, verify='cert.pem')
+            print("result = " + str(r))
 
 
 fp = open('config.json')
-data = json.loads(fp.read())
+config = json.loads(fp.read())
 fp.close()
 
+print("config = " + str(config))
 
-for cdir in data.dirs:
-    copy_over(cdir)
+hosts = config['hosts']
+
+for cdir in config['dirs']:
+    install_directory(hosts, cdir)
