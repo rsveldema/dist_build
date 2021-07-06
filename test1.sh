6dc6bced532279bbce0b99055f28ce42ba23ef0f
ronald-veldema <ronald.veldema@zuehlke.com>
2021-07-06 23:46:41 +0200

    test compile flag passing for msvc

---
+++ test1.sh
@@ -0,0 +1,9 @@
+
+export MSYS_NO_PATHCONV=1
+
+python dist_build.py /Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Community/VC/Tools/MSVC/14.29.30037/bin/Hostx64/x64/cl.exe \
+      /I /Program\ Files\ \(x86\)/Windows\ Kits/10/Include/10.0.19041.0/ucrt \
+      /I /Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio/2019/Community/VC/Tools/MSVC/14.29.30037/include \
+      /c \
+     /Fo"build/" /EHsc \
+      tests/hello.c
