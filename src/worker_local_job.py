21b178632076652a56d147ffd3141045f5f7d53b
Ronald Veldema <rsveldema@gmail.com>
2021-07-18 22:48:19 -0700

    - use absolate paths in generated cmdlist - keep both original and new cmdlist

--- src/worker_local_job.py
+++ src/worker_local_job.py
@@ -11,18 +11,51 @@ import json
 import logging
 import asyncio
 
+def get_output_file_from_list(cmdlist):
+    gcc_output_path = "-o"
+    msvc_output_path = "/Fo"
+
+    i = 0
+    while i < len(cmdlist):
+        param = cmdlist[i]
+        if param == gcc_output_path:
+            i += 1
+            param = cmdlist[i]
+            # todo: validate param
+            return param
+        elif param.startswith(gcc_output_path):
+            return param[len(gcc_output_path):]
+        elif param.startswith(msvc_output_path) and not is_a_directory_path(param):
+            return param[len(msvc_output_path):]
+        i += 1
+    return None
+
+def get_dep_file_from_cmdlist(cmdlist:List[str]) -> str:
+    i = 0
+    while (i < len(cmdlist)):
+        orig = cmdlist[i]
+        if orig == '-MF':
+            i += 1
+            orig = cmdlist[i]
+            return orig
+
+        i += 1
+    return None
+    
 class LocalBuildJob:
-    cmdlist: List[str]
+    original_cmdlist: List[str]
     id: str
     env: Dict[str, str]
     files: Dict[str, str]
     options: options.DistBuildOptions
     user_include_roots: List[str]
+    cached_current_dir: str
     profiler: profiler.Profiler
 
     def __init__(self, cmdline: List[str], env: str, id:str, client_sslcontext, session, files: Dict[str, str], options: options.DistBuildOptions, user_include_roots: List[str], profiler: profiler.Profiler):
-        self.cmdlist = json.loads(cmdline)
+        self.original_cmdlist = json.loads(cmdline)
         self.env = json.loads(env)
+        self.cached_current_dir = None
         self.profiler = profiler
         self.id = id
         self.files = json.loads(files)
@@ -47,17 +80,18 @@ class LocalBuildJob:
 
     async def run(self):
         profiler.notify_new_job_started()
-        self.change_dir()
-        self.save_files()
-        self.change_include_dirs()
-        self.change_debug_dirs()
-        self.change_output_dirs()
-        (retcode, result) = await self.exec_cmd()        
-        await self.send_reply(retcode, result)
+        new_cmd_list = self.save_files(self.original_cmdlist)
+        new_cmd_list = self.change_include_dirs(new_cmd_list)
+        new_cmd_list = self.change_debug_dirs(new_cmd_list)
+        new_cmd_list = self.change_output_dirs(new_cmd_list)
+        (retcode, result) = await self.exec_cmd(new_cmd_list)        
+        await self.send_reply(retcode, result, new_cmd_list)
         profiler.notify_job_done()        
-        #os.chdir(source_storage_dir(self.username))
  
     def get_current_dir_from_env(self) ->str:
+        if self.cached_current_dir != None:
+            return self.cached_current_dir
+
         found_env_cwd:str = None
         if "CWD" in self.env:
             found_env_cwd = self.env["CWD"]
@@ -69,22 +103,17 @@ class LocalBuildJob:
             logging.debug("FOUND CWD/PWD in sent env: " + found_env_cwd)
             found_env_cwd = source_storage_dir(self.username) + '/' + found_env_cwd
             os.makedirs(found_env_cwd, exist_ok=True)
+    
+        self.cached_current_dir = found_env_cwd
         return found_env_cwd
 
-    def change_dir(self):
-        cwd:str = self.get_current_dir_from_env()
-        if cwd != None:
-            logging.info("CHDIR: " + cwd)
-            os.chdir(cwd)
-        else:
-            print("failed to find CWD or PWD in env. variables. Can't change to original build dir in sandbox to allow relative includes to work")
 
-    def change_debug_dirs(self):
+    def change_debug_dirs(self, cmdlist: List[str]) -> List[str]:
         # when seeing: /FdCMakeFiles\cmTC_ea5a2.dir
         # we need to create this dir in the build dir         
         new_cmdline: List[str] = []
         opt_prefix = '/Fd'
-        for orig in self.cmdlist:       
+        for orig in cmdlist:       
             if orig.startswith(opt_prefix):
                 orig = orig[len(opt_prefix):]
                 orig = uniform_filename(orig)
@@ -92,24 +121,20 @@ class LocalBuildJob:
                 orig = opt_prefix + new_debug_dir
                 os.makedirs(new_debug_dir, exist_ok=True)
             new_cmdline.append(orig)   
-        self.cmdlist = new_cmdline
+        return new_cmdline
 
 
-    def get_dependency_file(self):
-        i = 0
-        while (i < len(self.cmdlist)):
-            orig = self.cmdlist[i]
-            if orig == '-MF':
-                i += 1
-                orig = self.cmdlist[i]
-            
-                new_dep_dir = orig
-                return new_dep_dir
+    def get_original_dependency_file(self):
+        return get_dep_file_from_cmdlist(self.original_cmdlist)
 
-            i += 1
+    def get_local_dependency_file(self, cmdlist: List[str]) -> str:
+        filename = get_dep_file_from_cmdlist(cmdlist)
+        if filename != None:
+            new_dep_dir = self.relative_to_abs_dir(filename)
+            return new_dep_dir
         return None
 
-    def change_output_dirs(self):
+    def change_output_dirs(self, cmdlist:List[str]) -> List[str]:
         # when seeing: /FdCMakeFiles\cmTC_ea5a2.dir
         # we need to create this dir in the build dir         
         new_cmdline:List[str] = []
@@ -117,52 +142,69 @@ class LocalBuildJob:
         opt_prefix_GCC = '-o'
 
         i = 0
-        while (i < len(self.cmdlist)):
-            orig = self.cmdlist[i]
+        while (i < len(cmdlist)):
+            orig = cmdlist[i]
 
             if orig == '-MF':
                 new_cmdline.append(orig)  
                 i += 1
-                orig = self.cmdlist[i]
+                orig = cmdlist[i]
             
                 new_dep_dir = get_all_but_last_path_component(orig)
                 os.makedirs(new_dep_dir, exist_ok=True)
-
-            if orig == opt_prefix_GCC:
+                orig = self.relative_to_abs_dir(orig)
+            elif orig == opt_prefix_GCC:
                 new_cmdline.append(orig)  
                 i += 1
-                orig = self.cmdlist[i]
-                new_output_file = uniform_filename(orig)               
-                new_output_file = get_all_but_last_path_component(new_output_file)
-                os.makedirs(new_output_file, exist_ok=True)
+                orig = cmdlist[i]
+                orig = self.relative_to_abs_dir(orig)
+
+                new_output_dir = get_all_but_last_path_component(orig)
+                os.makedirs(new_output_dir, exist_ok=True)
             elif orig.startswith(opt_prefix_VC):
                 orig = orig[len(opt_prefix_VC):]
-                new_debug_dir = uniform_filename(orig)
-                #new_debug_dir = source_storage_dir(self.username) + '/' + orig
-                orig = opt_prefix_VC + new_debug_dir
+                orig = self.relative_to_abs_dir(orig)
+                new_debug_dir = orig
+                orig = opt_prefix_VC + orig
                 if make_dir_but_last(new_debug_dir):
                     os.makedirs(new_debug_dir, exist_ok=True)
 
             new_cmdline.append(orig)  
-            i += 1 
+            i += 1
+        return new_cmdline
 
-        self.cmdlist = new_cmdline
 
-    def change_include_dirs(self):
+
+    def relative_to_abs_dir(self, path:str) -> str:
+        path = uniform_filename(path)
+        if path.startswith('/') or path.startswith('\\'):
+            return path
+        path = self.get_current_dir_from_env() + '/' + path
+        return path
+
+
+    def change_include_dirs(self, cmdlist: List[str]) -> List[str]:
         new_cmdline:List[str] = []
-        found_include_directive_for_next_option = False
-        for orig in self.cmdlist:
+        i = 0
+        while i < len(cmdlist):
+            orig = cmdlist[i]
             new_cmd = orig
-            if found_include_directive_for_next_option:
-                found_include_directive_for_next_option = False
+
+            if orig == '/I' or orig == '-I':
+                # exactly -I means that the next item will be the path
+                new_cmdline.append(new_cmd) 
+                i += 1
+                    
+                orig = cmdlist[i]
+                new_cmd = orig
+                
                 #print(f"TEST ME HERE {orig}")
                 if self.is_user_directory(orig):
                     orig = uniform_filename(orig) 
                     new_cmd = source_storage_dir(self.username) + orig
-            if orig == '/I' or orig == '-I':
-               found_include_directive_for_next_option = True
+                else:
+                    new_cmd = self.relative_to_abs_dir(orig)
             elif orig.startswith('-I'):
-                no_replacement = orig
                 orig = orig[2:]
 
                 #print(f"TEST ME HERE2 {orig}")
@@ -170,32 +212,34 @@ class LocalBuildJob:
                     orig = uniform_filename(orig)
                     new_cmd = '-I' + source_storage_dir(self.username) + orig    
                 else:
-                    new_cmd = no_replacement      
+                    new_cmd = '-I' + self.relative_to_abs_dir(orig)
 
             new_cmdline.append(new_cmd)   
-        self.cmdlist = new_cmdline
+            i += 1
+        return new_cmdline
 
 
-    def save_files(self):
+    def save_files(self, cmdlist: List[str]) -> List[str]:
         self.profiler.enter()
         #print(str(self.files))
         for it in self.files:
             oldpath = it
             newpath = self.save_file(oldpath, self.files[it])
             #print(f"SAVE FILES ====> {oldpath} vs {newpath}")
-            #self.patch_arg_refering_saved_file(oldpath, newpath)
+            cmdlist = self.patch_arg_refering_saved_file(oldpath, newpath, cmdlist)
         self.profiler.leave()
+        return cmdlist
 
-    def patch_arg_refering_saved_file(self, oldpath:str, newpath:str):
+    def patch_arg_refering_saved_file(self, oldpath:str, newpath:str, cmdlist: List[str]) -> List[str]:
         #print("PATCH: " + oldpath + ' -> ' + newpath)
         new_cmdline = []
-        for orig in self.cmdlist:
+        for orig in cmdlist:
             if orig == oldpath:
                 fixed = orig.replace(oldpath, newpath)
             else:
                 fixed = orig
             new_cmdline.append( fixed )
-        self.cmdlist = new_cmdline
+        return new_cmdline
 
     def save_file(self, old_path:str, content) -> str:
         if old_path.startswith('.'):
@@ -211,36 +255,28 @@ class LocalBuildJob:
         return container_path
 
 
-    async def exec_cmd(self) -> Tuple[int, str]:
+    async def exec_cmd(self, new_cmd_list: List[str]) -> Tuple[int, str]:
         self.profiler.enter()
         exit_code = -1
         stderr = ""
         stdout = ""
         try:
-            logging.info("EXEC: " + str(self.cmdlist))
+            logging.info("EXEC: " + str(new_cmd_list))
 
-            program = self.cmdlist[0]
-            args = self.cmdlist[1:]
-            proc:Process = await asyncio.subprocess.create_subprocess_exec(stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE, *self.cmdlist )
-            #print("trying to wait")
-            #exit_code = await proc.wait()
-            #stdout, stderr = await asyncio.wait_for(proc.communicate(), 1000)
+            proc:Process = await asyncio.subprocess.create_subprocess_exec(stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE, *new_cmd_list )
             stdout, stderr = await proc.communicate()
-            #proc.terminate()
-            #await proc.wait()
-            #await asyncio.sleep(0.5)
 
             exit_code = proc.returncode
-            print(f"got stdout {stdout}, ret {exit_code} for {self.cmdlist}")
+            print(f"got stdout {stdout}, ret {exit_code} for {new_cmd_list}")
 
             stdout = stdout.decode()
             stderr = stderr.decode()
 
         except FileNotFoundError as e:
-            stderr = f"failed to find file: {self.cmdlist}"
+            stderr = f"failed to find file: {new_cmd_list}"
             logging.error(stderr)
         except Exception as e:
-            stderr = f"unknown error during run of {self.cmdlist}, {e}"
+            stderr = f"unknown error during run of {new_cmd_list}, {e}"
             logging.error(stderr)
 
         result_data = {
@@ -252,56 +288,50 @@ class LocalBuildJob:
         self.profiler.leave()
         return (exit_code, json.dumps(result_data))
         
-    def get_explicit_output_file(self):
-        gcc_output_path = "-o"
-        msvc_output_path = "/Fo"
 
-        next_is_winner = False
-        for param in self.cmdlist:
-            if next_is_winner:
-                return param
-            if param == gcc_output_path:
-                next_is_winner = True
-                pass
-            elif param.startswith(gcc_output_path):
-                return param[len(gcc_output_path):]
-            elif param.startswith(msvc_output_path) and not is_a_directory_path(param):
-                return param[len(msvc_output_path):]
-        return None
+    def get_local_output_file(self, cmdlist):
+        return get_output_file_from_list(cmdlist)
+    
+    def get_original_output_file(self):
+        return get_output_file_from_list(self.original_cmdlist)
 
-
-    def get_output_path(self):        
+    def get_original_output_path(self):        
         vs_output_path = "/Fo"
-        for param in self.cmdlist:
+        for param in self.original_cmdlist:
             if param.startswith(vs_output_path) and is_a_directory_path(param):
                 return param[len(vs_output_path):]
         return None
 
     def is_using_microsoft_compiler(self):
-        compiler_name = self.cmdlist[0].lower()
+        compiler_name = self.original_cmdlist[0].lower()
         if compiler_name.endswith("cl.exe"):
             return True        
         return False
 
-    async def append_output_files(self, outfiles: Dict[str, bytes]):
-        dependency_file = self.get_dependency_file()
-        if dependency_file != None:
-            file_content = safe_read_binary_content(dependency_file)
-            if file_content != None:
-                logging.debug("returning dependency file " + dependency_file)
-                outfiles[dependency_file] = file_content
+    async def append_output_files(self, outfiles: Dict[str, bytes], cmdlist:List[str]):
+        orig_dependency_file = self.get_original_dependency_file()
 
-        explicit_out = self.get_explicit_output_file()
-        if explicit_out != None:
-            file_content = safe_read_binary_content(explicit_out)
+        if orig_dependency_file != None:
+            local_dependency_file = self.get_local_dependency_file(cmdlist)
+            assert local_dependency_file != None
+            file_content = safe_read_binary_content(local_dependency_file)
             if file_content != None:
-                outfiles[explicit_out] = file_content
+                logging.debug("returning dependency file " + orig_dependency_file)
+                outfiles[orig_dependency_file] = file_content
+
+        orig_explicit_out = self.get_original_output_file()
+        if orig_explicit_out != None:
+            local_explicit_out = self.get_local_output_file(cmdlist)
+            assert local_explicit_out != None
+            file_content = safe_read_binary_content(local_explicit_out)
+            if file_content != None:
+                outfiles[orig_explicit_out] = file_content
             else:
-                logging.error("failed to safe-read: " + explicit_out)
+                logging.error("failed to safe-read: " + local_explicit_out)
             return
 
         is_microsoft = self.is_using_microsoft_compiler()
-        for p in self.cmdlist:
+        for p in self.original_cmdlist:
             if is_source_file(p):
                 output_file = transform_filename_to_output_name(p, is_microsoft, self.get_output_path())
                 #print("output file ==== " + output_file)
@@ -310,14 +340,14 @@ class LocalBuildJob:
                     outfiles[output_file] = read_binary_content(output_file)
 
 
-    async def send_reply(self, retcode, result:str):
+    async def send_reply(self, retcode, result:str, cmdlist:List[str]):
         self.profiler.enter()
         outfiles: Dict[str, bytes] = {}
 
         if retcode == 0:
-            await self.append_output_files(outfiles)
+            await self.append_output_files(outfiles, cmdlist)
         else:
-            print(f"-------------------------RETODE {retcode} OFF FOR {self.cmdlist}")
+            print(f"-------------------------RETODE {retcode} OFF FOR {cmdlist}")
 
         #print(f"output files are {outfiles}")
 
